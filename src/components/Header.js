cb58c7857646d56dbb6290134a8fa19456a1d62c
JrKh16 <JirawatKhunngean@gmail.com>
2024-02-09 21:14:24 +0700

    new my-app

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Header = () => {
+
+    const headerStyle = {
+
+        width: '100%',
+        padding: '2%',
+        backgroundColor: "lime",
+        color: 'black',
+        textAlign: 'center'
+
+    }
+
+    return(
+        <div style={headerStyle}>
+            <h1>Jirawat 007</h1>
+        </div>
+    )
+}
